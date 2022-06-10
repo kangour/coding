b9dadfce5cde0e17cb8971610777c0034094e010
kangour <kangour@sina.cn>
2022-06-10 15:00:18 +0800

    feature(tree/tree.py): 倒序遍历列表

--- code/tree/tree.py
+++ code/tree/tree.py
@@ -23,3 +23,16 @@ class TreeCase(unittest.TestCase):
         root.left = TreeNode(2)
         root.right = TreeNode(3)
         self.traverse(root)
+
+
+class ListCase(unittest.TestCase):
+    def traverse(self, data: list, index: int = 0):
+        """倒序打印列表"""
+        if index >= len(data):
+            return
+        self.traverse(data, index + 1)
+        logger.info(data[index])
+
+    def test(self):
+        data = [1, 2, 3, 4, 5]
+        self.traverse(data)
