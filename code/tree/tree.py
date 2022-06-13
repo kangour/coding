318d0a422667f7a5cdbdad234f8f7c49781960e2
kangour <kangour@sina.cn>
2022-06-13 16:35:09 +0800

    feature(tree/tree.py): BFS：递归分层遍历二叉树

--- code/tree/tree.py
+++ code/tree/tree.py
@@ -145,6 +145,46 @@ class TreeDepthCase(unittest.TestCase):
         logger.info(f"每一层的节点：{res}")
 
 
+class TreeBFSCase(unittest.TestCase):
+    """递归遍历，二叉树广度优先"""
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.bfs_nodes = []
+
+    def traverse(self, nodes: list):
+        if not nodes:
+            return
+
+        sub_nodes = []
+
+        for node in nodes:
+            if not node:
+                continue
+            self.bfs_nodes.append(node.value)
+
+            sub_nodes.append(node.left)
+            sub_nodes.append(node.right)
+
+        self.traverse(sub_nodes)
+
+        return self.bfs_nodes
+
+    def test_depth(self):
+        root = TreeNode(1)
+        root.left = TreeNode(2)
+        root.right = TreeNode(3)
+        root.left.left = TreeNode(4)
+        root.left.right = TreeNode(5)
+        root.right.left = TreeNode(6)
+        root.right.right = TreeNode(7)
+
+        data = [root]
+
+        res = self.traverse(data)
+        logger.info(f"BFS：递归分层遍历二叉树：{res}")
+
+
 class ListCase(unittest.TestCase):
     """列表打印"""
 
