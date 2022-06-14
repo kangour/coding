9d2efa571141c990c2ad222b866dc9c14bc46150
kangour <kangour@sina.cn>
2022-06-14 11:05:34 +0800

    feature(dp/fibonacci.py): 备忘录改良的斐波那契

--- code/dp/fibonacci.py
+++ code/dp/fibonacci.py
@@ -8,6 +8,20 @@ class Fibonacci(unittest.TestCase):
             return n
         return self.fib(n - 1) + self.fib(n - 2)  # 调了两次递归，造成重复的计算，可以用动态规划思想优化
 
+    @staticmethod
+    def fib_note(n: int):
+        """改良的斐波那契，用备忘录记录上一步的结果，避免重复计算"""
+        if n in [0, 1]:
+            return n
+        a, b = 0, 1
+        for i in range(n - 1):
+            b, a = a + b, b
+        return b
+
     def test_fib(self):
         res = self.fib(33)
         logger.info(f"Fibonacci：{res}")
+
+    def test_fib_note(self):
+        res = self.fib_note(33)
+        logger.info(f"Note Fibonacci：{res}")
